1c50b400f8565d3e4513df082bef9f1c9f2c4c91
Vijayavedhasekarn <vedhatech02@gmail.com>
2024-01-29 11:25:12 +0530

    production

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/The-Blog-React-App/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
 })
