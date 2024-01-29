fadb45317ce1797c29099569084a6bde2cd7bf28
Vijayavedhasekarn <vedhatech02@gmail.com>
2024-01-29 11:28:02 +0530

    fix: production error

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/The-Blog-React-App/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-})
+});
